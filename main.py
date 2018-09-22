fffe1424f0df71eaa9f58570642a0d30a34fdb05
Nihit Save <nihitsave15@gmail.com>
2018-09-22 09:52:23 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import cv2
 import os
 import googletrans
 import glob
-#os.chdir('/home/kneehit/Data Science/Avito Ad Demand/Avito')
-
+os.chdir('/home/kneehit/Data Science/Avito Ad Demand/Avito')
+import random
 
 
 #%%
@@ -29,9 +29,6 @@ train_active = pd.read_csv('train_active.csv',nrows = 100)
 test_active = pd.read_csv('test_active.csv',nrows = 100)
 
 #%%
-translator = googletrans.Translator()
-a = translator.translate('안녕하세요.')
-#%%
 train_images_path = 'train/data/competition_files/train_jpg/'
 test_images_path = 'test/data/competition_files/test_jpg/'
 
@@ -39,3 +36,32 @@ train_images = glob.glob(train_images_path +'*.jpg')
 test_images = glob.glob(test_images_path +'*.jpg')
 
 #%%
+
+def visualize_translated(num = random.randint(0,train.shape[0])):
+    translator = googletrans.Translator()
+    item_translated = {}
+    item_translated['region'] = translator.translate(train.iloc[num,2]).text
+    item_translated['city'] = translator.translate(train.iloc[num,3]).text
+    item_translated['parent_cat'] = translator.translate(train.iloc[num,4]).text
+    item_translated['category'] = translator.translate(train.iloc[num,5]).text
+    item_translated['param_1'] = translator.translate(train.iloc[num,6]).text if not pd.isna(train.iloc[num,6]) else 'NA'
+    item_translated['param_2'] = translator.translate(train.iloc[num,7]).text if not pd.isna(train.iloc[num,7]) else 'NA'
+    item_translated['param_3'] = translator.translate(train.iloc[num,8]).text if not pd.isna(train.iloc[num,8]) else 'NA'
+    item_translated['title']= translator.translate(train.iloc[num,9]).text
+    item_translated['desc'] = translator.translate(train.iloc[num,10]).text
+    print(item_translated)
+    
+    
+    if not pd.isna(train.iloc[num,15]):
+        image_path = train_images_path + train.iloc[num,15] + '.jpg'
+        a = cv2.imread(image_path)
+        cv2.imshow('',a)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print('\nImage Missing')
+
+visualize_translated(21)
+
+
+
