00f5b392ba296dc2a3c3bfd6f2f0de4b69d1e104
Nihit Save <nihitsave15@gmail.com>
2018-10-27 22:39:37 -0500

    Update scrapper.py

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -18,23 +18,24 @@ wiki.set_lang('ru')   # else gives incorrect results Eg. for 'Самара' gave
 a = wiki.page('Самара')
 a_html = a.html()
 
-#%%
-a_soup = soup(a_html,'html.parser')
-a_pop =  a_soup.find_all(id = 'Население')[0]
+#%% --------------------- Trial Script ----------------------
+# a_soup = soup(a_html,'html.parser')
+# a_pop =  a_soup.find_all(id = 'Население')[0]
 #%%
 # For table 
-tab = a_pop.findNext(class_ = 'standard')
+# tab = a_pop.findNext(class_ = 'standard')
 # For Year
-year_row = tab.find_all(class_ = 'bright')[-1]
-year = year_row.find_all('th')[-2].text
+# year_row = tab.find_all(class_ = 'bright')[-1]
+# year = year_row.find_all('th')[-2].text
 # For population count
-pop_row = tab.find_all(align = 'center')[-1]
-pop = pop_row.find_all('td')[-2].text
+# pop_row = tab.find_all(align = 'center')[-1]
+# pop = pop_row.find_all('td')[-2].text
 
 # \xa0 is non-breaking space in Latin1 encoding
-pop_clean = pop.replace(u'\xa0',u'') 
+# pop_clean = pop.replace(u'\xa0',u'') 
 
-#%%
+#%% --------------------- Trial Script ----------------------
+
 city_counts = train['city'].value_counts() 
 unique_cities = list(city_counts.index)
 
