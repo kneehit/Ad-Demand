20e4e1223c03083ab68daf632b3e56fd016bfaea
Nihit Save <nihitsave15@gmail.com>
2018-11-07 21:07:03 -0600

    Update scrapper.py

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -103,15 +103,19 @@ for ind in range(len(unique_cities)):
 city_pop.to_csv('Population Dirty.csv')
 #%%
 # Example of values in population column - '↗1 468 833[2] человека (2018)'
-# Split the string by '[' so that we get '↗1 468 833'
+# Split the string by '[' so that 
 city_pop['popu_string'] = city_pop['popu_string'].str.split('[').str[0]
+# Our values now look like '↗1 468 833'
 
 # The space in the string is actually \xa0 which is non-breaking space in Latin1 encoding
 # Some values also have the space character ' ' as well.
 # Therefore we replace both of them by ''
 city_pop['popu_string'] = city_pop['popu_string'].str.replace('\xa0','').str.replace(' ','')
+# Now most of the values look like this '↗1468833'
 
+# To extract the population count we will use the regex for digits - '\d'
 city_pop['popu_string'] = city_pop['popu_string'].str.extract('(\d+)')
+# 
 city_pop['popu_string'] = pd.to_numeric(city_pop['popu_string'])
 
 city_pop.to_csv('Population Clean.csv')
