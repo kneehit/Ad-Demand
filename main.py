8691277743582c906ce0b5672af066d04c2be770
Nihit Save <nihitsave15@gmail.com>
2019-01-01 21:10:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -147,6 +147,7 @@ train['description'][random.randint(0,train.shape[0])]
 chars_to_replace = ['/\n','№','Б/у','\n','☎','✔','✘','☛','☚','•','«','»']
 # List of punctuation marks
 chars_to_replace.extend(list(string.punctuation))
+# List of commonly occuring fractions
 chars_to_replace.extend(['¹', '²', '³', '½', '⅓', '¼', '⅕', '⅙', '⅐', '⅛', '⅑', '⅒', '⅔', '⅖', '¾', '⅗', '⅜', '⅘', '⅚', '⅝', '⅞'])
 # Numbers can occur in various forms in the item description.
 chars_to_replace.extend(['⓵','⓶','⓷','⓸','⓹','⓺','⓻','⓼','⓽','⓾'])
